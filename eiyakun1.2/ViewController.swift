f9fafa4751db0c2352491716a55b53edde15971f
kotarokobayashi45 <kobakoba588445@yahoo.co.jp>
2017-03-16 11:49:27 +0900

    Initial Commit

---
+++ eiyakun1.2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eiyakun1.2
+//
+//  Created by 小林光太郎 on 2017/03/16.
+//  Copyright © 2017年 kotarou.kobayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
